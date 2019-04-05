160bd9313272307208007dc2b7d6fc3d70b7d39d
xinaiz <dominic.jurczak@yahoo.com>
2019-04-05 11:36:10 -0700

    add evil annotation to cast function

--- src/main/kotlin/com/xinaiz/evilkotlin/casts/Casts.kt
+++ src/main/kotlin/com/xinaiz/evilkotlin/casts/Casts.kt
@@ -1,7 +1,12 @@
 package com.xinaiz.evilkotlin.casts
 
+import com.xinaiz.evilkotlin.qualifiers.Evilness
+import com.xinaiz.evilkotlin.qualifiers.HowEvil
+
 /**
  * Cast object to desired type without added parenthesis layer.
  * This might cause programmer's IDE to lose some insight into the cast
  */
+
+@Evilness(HowEvil.PRETTY_EVIL)
 inline fun <reified T> Any.cast() = this as T
