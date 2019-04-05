2c61e4e237756d84b3cb345ef09a3f8aff77aa1f
xinaiz <dominic.jurczak@yahoo.com>
2019-04-05 11:21:00 -0700

    add tryOrDefault overloads

--- src/main/kotlin/com/xinaiz/evilkotlin/infoloss/InfoLoss.kt
+++ src/main/kotlin/com/xinaiz/evilkotlin/infoloss/InfoLoss.kt
@@ -5,20 +5,35 @@ import com.xinaiz.evilkotlin.qualifiers.HowEvil
 
 /**
  * Try to execute block.
- * If something bad happens ¯\_(ツ)_/¯ return a default value.
+ * If something bad happens ¯\_(ツ)_/¯ return a default expression.
  * YOU LOSE:
  * 1. The cause what happened
  * 2. Dignity
  */
-
 @Evilness(HowEvil.VERY_EVIL)
-fun <T> tryOrDefault(block: () -> T, default: T)
-    = try { block() } catch (ex: Throwable) { default }
+fun <T> tryOrDefault(block: () -> T, default: () -> T) = try {
+  block()
+} catch (ex: Throwable) {
+  default()
+}
 
 /**
- * The same as [tryOrDefault], but accepts expression for default value
+ * Try to execute block.
+ * If something bad happens ¯\_(ツ)_/¯ return a default value.
  */
 @Evilness(HowEvil.VERY_EVIL)
-fun <T> tryOrDefault(block: () -> T, default: () -> T)
-    = try { block() } catch (ex: Throwable) { default() }
+fun <T> tryOrDefault(block: () -> T, default: T) = tryOrDefault(block) { default }
 
+/**
+ * Try to access variable.
+ * If something bad happens ¯\_(ツ)_/¯ return a default expression.
+ */
+@Evilness(HowEvil.VERY_EVIL)
+fun <T> tryOrDefault(property: T, default: () -> T) = tryOrDefault({ property }, default)
+
+/**
+ * Try to access variable.
+ * If something bad happens ¯\_(ツ)_/¯ return a default value.
+ */
+@Evilness(HowEvil.VERY_EVIL)
+fun <T> tryOrDefault(property: T, default: T) = tryOrDefault(property) { default }
